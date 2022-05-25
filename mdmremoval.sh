5b3f69db70121df1f7b5fad79f0d2537c0487f3c
ChazzaH014 <47654474+ChazzaH014@users.noreply.github.com>
2022-05-25 10:41:43 +1000

    Update mdmremoval.sh

--- mdmremoval.sh
+++ mdmremoval.sh
@@ -1,10 +1,7 @@
 #!/bin/sh
 cd ~
-echo Creating work directory
 mkdir mdmscript/
 cd mdmscript/
-echo Retrieving files
 wget https://github.com/ChazzaH014/mdm-removal-script/blob/main/CloudConfigurationDetails.plist
-echo Patching MDM Profile
 mv CloudConfigurationDetails.plist /var/containers/Shared/SystemGroup/systemgroup.com.apple.configurationprofiles/Library/ConfigurationProfiles
-echo Done
+
